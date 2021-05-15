7317405e688fe17139bb7547d31d353d587847d9
hasecom <hasetennisth@gmail.com>
2021-05-15 22:18:35 +0900

    Propsテスト

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Props = {
+  message:string,
+  children:React.ReactNode
 }
-
-export default App;
+const Child : React.FC<Props> = ({message,children}) => {
+  return (
+    <p>{message}の{children}</p>
+  )
+}
+ 
+const App: React.FC = () => {
+    return (
+      <div className="App">
+          <Child message="1">子コンポーネント</Child>
+          <Child message="2">子コンポーネント</Child>
+      </div>
+    )
+}
+ 
+export default App
