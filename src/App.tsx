9ef1333d961cbfebed5d498202a472de78671354
hasecom <hasetennisth@gmail.com>
2021-05-15 22:32:55 +0900

    file section

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import Child from "./components/Child"
 import './App.css';
 
-type Props = {
-  message:string,
-  children:React.ReactNode
-}
-const Child : React.FC<Props> = ({message,children}) => {
-  return (
-    <p>{message}の{children}</p>
-  )
-}
+
  
 const App: React.FC = () => {
     return (
       <div className="App">
-          <Child message="1">子コンポーネント</Child>
-          <Child message="2">子コンポーネント</Child>
+        <Child />
       </div>
     )
 }
---
+++ src/components/Child.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Child:React.FC = () => {
+  return (
+    <div>子コンポーネントですよ</div>
+  )
+}
+export default Child
