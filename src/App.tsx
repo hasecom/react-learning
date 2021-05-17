973020019cc55f3db78e0c6fbb94acab4c4066c5
hasecom <hasetennisth@gmail.com>
2021-05-17 23:09:56 +0900

    ページ分割

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import React,{useState} from "react"
-import {Child1,Child2} from "./components/Child"
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Home from "./scenes/Home"
+import About from "./scenes/About"
 
- 
-const App: React.FC = () => {
-  const [count,setCount] = useState<number>(0)
-  return (
-    <div className="App">
-      <div>{count}</div>
-      <button onClick={()=>setCount(count+1)}>+1</button>
-      <button onClick={()=>setCount(count-1)}>-1</button>
-      <Child1 />
-      <Child2 />
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+      </ul>
+
+      <hr />
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
     </div>
-  )
-}
- 
+  </BrowserRouter>
+)
+
+
 export default App
