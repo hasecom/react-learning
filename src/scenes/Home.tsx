63f1fea22964ccab4cd9b2a602efcc7ca887f2f8
hasecom <hasetennisth@gmail.com>
2021-05-18 23:14:34 +0900

    build

--- src/scenes/Home.tsx
+++ src/scenes/Home.tsx
@@ -2,7 +2,11 @@ import React , {Component} from "react"
 class About extends Component{
   render(){
     return(
-      <div>これはHOME</div>
+      <div>これはHOME
+        <div>
+          <a target="_blank" href="https://qiita.com/tk-r1d3r/items/aec311c78846f7c9bf04">参考先</a>
+        </div>
+      </div>
     )
   }
 }
