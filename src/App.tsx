2f8582d03666870418877d6e88fdb806899e661e
hasecom <hasetennisth@gmail.com>
2021-05-23 23:19:51 +0900

    gitHub PagesにはHashRouterを採用

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ const App = () => (
     <div>
       {process.env.PUBLIC_URL}
       <ul>
-        <li><Link to=''>Home</Link></li>
-        <li><Link to='about'>About</Link></li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
       </ul>
 
       <hr />
-      <Route exact path='' component={Home} />
-      <Route path='about' component={About} />
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
     </div>
   </HashRouter>
 )
